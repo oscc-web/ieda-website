c06953c0571ec9780735a4cb86909a279352eec8
myyerrol <myyerrol@126.com>
2023-10-08 23:02:52 +0800

    feat: add element-plus and vue

--- src/.vuepress/client.ts
+++ src/.vuepress/client.ts
@@ -1,12 +1,19 @@
 import { defineClientConfig } from "@vuepress/client";
+import ElementPlus from "element-plus";
+import * as ElementPlusIconsVue from "@element-plus/icons-vue";
+import "element-plus/dist/index.css";
 
 export default defineClientConfig({
     enhance({ app, router, siteData }) {
+        app.use(ElementPlus);
+        Object.keys(ElementPlusIconsVue).forEach(key => {
+            app.component(key, ElementPlusIconsVue[key]);
+        });
         router.afterEach((to) => {
             if (to.path === "/") {
             }
         });
     },
     setup() {},
-    rootComponents: [],
+    rootComponents: []
 });
--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,4 +1,15 @@
+import path from "path";
 import { defineUserConfig } from "vuepress";
+import { registerComponentsPlugin } from "@vuepress/plugin-register-components";
+
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
+import Icons from "unplugin-icons/vite"
+import IconsResolver from "unplugin-icons/resolver"
+
+import { searchProPlugin } from "vuepress-plugin-search-pro";
+
 import theme from "./theme.js";
 
 export default defineUserConfig({
@@ -10,5 +21,53 @@ export default defineUserConfig({
             description: "iEDA的官方网站",
         }
     },
+    shouldPrefetch: false,
+    plugins: [
+        AutoImport({
+            imports: ["vue"],
+            resolvers: [
+                ElementPlusResolver(),
+                IconsResolver({
+                    prefix: "Icon"
+                })
+            ]
+        }),
+        Components({
+            resolvers: [
+                ElementPlusResolver(),
+                IconsResolver({
+                    enabledCollections: ["ep"]
+                })
+            ]
+        }),
+        Icons({
+            autoInstall: true
+        }),
+        registerComponentsPlugin({
+            componentsDir: path.resolve(__dirname, "./components")
+        }),
+        searchProPlugin({
+            indexContent: true,
+            autoSuggestions: true,
+            customFields: [{
+                getter: (page) => page.frontmatter.title,
+                formatter: "标题：$content"
+            }, {
+                getter: (page) => page.frontmatter.author,
+                formatter: "作者：$content"
+            }, {
+                getter: (page) => page.frontmatter.category,
+                formatter: "分类：$content"
+            }, {
+                getter: (page) => page.frontmatter.tag,
+                formatter: "标签：$content"
+            }],
+            hotKeys: [{ key: "k", ctrl: true }, { key: "/", ctrl: true }],
+            queryHistoryCount: 5,
+            resultHistoryCount: 5,
+            searchDelay: 150,
+            sortStrategy: "max"
+        })
+    ],
     theme
 });
