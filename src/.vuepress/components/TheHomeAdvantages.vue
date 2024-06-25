a1cfc04cd84fca62b41773eb8ec6f365de15b0c8
Xingquan-Li <fzulxq@gmail.com>
2024-06-25 14:06:44 +0800

    complete ieda.oscc.cc web-1.0

--- src/.vuepress/components/TheHomeAdvantages.vue
+++ src/.vuepress/components/TheHomeAdvantages.vue
@@ -1,6 +1,89 @@
+
 <template>
-    
-    <!-- 关于iEDA -->
+  <!-- 关于iEDA -->
+  <section class="section bg-default section-md">
+    <div class="container">
+      <div class="row row-30">
+        <div class="col-md-6">
+          <h2 class="title-icon">
+            <span class="icon icon-default mercury-icon-target-2"></span>
+            <span class="text-light">
+              关于iEDA              
+              <a class="button button-primary" href="/project/intro/"> 查阅更多</a>
+            </span>
+          </h2>
+        </div>
+        <div class="col-md-6">
+          <div class="counter-container">
+            <div class="counter-item" v-for="(item, index) in counters" :key="index">
+              <div class="box-counter">
+                <div class="box-counter-main">
+                  <div class="counter">{{ item.value }}</div>
+                </div>
+                <p class="box-counter-title" v-html="item.title"></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      counters: [
+        { value: 1, title: '设计1个EDA公共基础底座<br>包含：解析器，数据库，管理平台，交互引擎' },
+        { value: 11, title: '完成11款EDA工具<br>包含：iMap、iNO、iFP、iPDN、iPL、iCTS、iTO、iRT、iSTA、iPA(iIR)、iDRC等' },
+        { value: 3, title: '通过iEDA工具成功进行3次流片<br>2次110nm工艺，1次28nm工艺' },
+        { value: 400, title: '在开源社区github和gitee<br>累计超400个标星' },
+        { value: 300000, title: 'iEDA代码量超过30万行<br>(不包括第三方库和历史)' },
+        { value: 26000, title: 'B站iEDA-视频号<br>已累计播放超26000次' }
+      ]
+    };
+  }
+};
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap');
+
+
+
+.counter-container {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 1rem;
+}
+
+.counter-item {
+  background-color: #fff;
+  border: 1px solid #ffffff;
+  border-radius: 0.5rem;
+  padding: 1rem;
+  text-align: center;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+}
+
+.box-counter-main .counter {
+  font-size: 2rem;
+  font-weight: 700;
+  color: #333;
+}
+
+.box-counter-title {
+  margin-top: 0.5rem;
+  font-size: 1rem;
+  color: #666;
+}
+</style>
+
+
+
+
+<!-- <template>
     <section class="section bg-default section-md">
         <div class="container">
             <div class="row row-30">
@@ -61,4 +144,4 @@
   <style scoped>
   /* CSS styles here */
   </style>
-  
+   -->
--- src/.vuepress/components/TheHomeCarousel.vue
+++ src/.vuepress/components/TheHomeCarousel.vue
@@ -1,12 +1,83 @@
 <template>
-    <el-carousel :interval="5000" type="card" height="300px">
-      <el-carousel-item v-for="item in 6" :key="item">
-        <h3 text="2xl" justify="center">{{ item }}</h3>
-      </el-carousel-item>
-    </el-carousel>
-  </template>
-  
-  <style scoped>
+  <el-carousel :interval="5000" type="card" height="300px">
+    <el-carousel-item v-for="(item, index) in imageItems" :key="index">
+      <div class="carousel-container">
+        <img :src="item.url" alt="Carousel Image" class="carousel-image"/>
+        <div class="carousel-text">{{ item.text }}</div>
+      </div>
+    </el-carousel-item>
+  </el-carousel>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      imageItems: [
+        { url: '/res/images/publicity/ISEDA/tutorial.jpg', text: 'iEDA团队在ISEDA 2024介绍Tutorial' },
+        { url: '/res/images/index/logo_place.gif', text: '用iEDA工具做的芯片版图logo' },
+        { url: '/res/images/index/home/IMG_4033.png', text: 'Andrew B. Kahng在介绍国际知名的开源EDA工具' },
+        { url: '/res/images/index/home/wh-ab.jpg', text: 'iEDA团队成员王昊和Andrew B. Kahng在ETH的合影' },
+        // { url: '/res/images/index/iEDA-tapeout.png', text: '三次流片成功' }
+      ]
+    };
+  }
+};
+</script>
+
+<style scoped>
+.carousel-container {
+  position: relative;
+  width: 100%;
+  height: 100%;
+}
+
+.carousel-image {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+
+.carousel-text {
+  position: absolute;
+  bottom: 10px;
+  left: 10px;
+  color: white;
+  background-color: rgba(119, 116, 116, 0.5);
+  padding: 6px;
+  border-radius: 2px;
+}
+</style>
+
+<!-- 
+<template>
+  <el-carousel :interval="5000" type="card" height="300px">
+    <el-carousel-item v-for="(item, index) in imageUrls" :key="index">
+      <img :src="item" alt="Carousel Image" class="carousel-image"/>
+      <h3 text="2xl" justify="center">{{ item }}</h3>
+    </el-carousel-item>
+  </el-carousel>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      imageUrls: [
+        // '/res/images/index/iEDA-tapeout.png',
+        // '/res/images/index/main-picture.png',
+        '/res/images/publicity/ISEDA/tutorial.jpg',
+        '/res/images/index/logo_place.gif',
+        '/res/images/index/home/IMG_4033.png',
+        '/res/images/index/home/wh-ab.jpg',
+        // 'https://example.com/image6.jpg'
+      ]
+    };
+  }
+};
+</script>
+
+<style scoped>
   .el-carousel__item h3 {
     color: #475669;
     opacity: 0.75;
@@ -22,7 +93,15 @@
   .el-carousel__item:nth-child(2n + 1) {
     background-color: #d3dce6;
   }
-  </style>
+
+  .carousel-image {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+</style> -->
+
+  
 
 <!--   
   
